aaf441bdd4c61b39a5052c9aa93e61c0ca68f6b1
DHniyeo <64246124+DHniyeo@users.noreply.github.com>
2024-09-08 16:05:53 +0900

    Update and rename (임시)마법의 숲 탐색_여동훈.cpp to 마법의 숲 탐색_여동훈.cpp

--- Samsung/마법의 숲 탐색_여동훈.cpp
+++ Samsung/마법의 숲 탐색_여동훈.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 #include<string.h>
 using namespace std;
 
@@ -40,7 +41,7 @@ bool move_block(int idx, int dir) {
 		if (ny >= R + 3 || nx < 0 || nx >= C) {
 			return false;
 		}
-		if (map[ny][nx] != 0 && map[ny][nx]!=idx+1) { // 해당 칸에 이미 있음
+		if (map[ny][nx] != 0 && map[ny][nx] != idx + 1) { // 해당 칸에 이미 있음
 			return false;
 		}
 		if (dir == 1 || dir == 3) { // 아래로 내려갈 수 있는지 확인
@@ -80,7 +81,7 @@ bool move_block(int idx, int dir) {
 	for (int i = 0; i < 4; i++) {
 		map[blocks[idx].exit[i].r][blocks[idx].exit[i].c] = idx + 1;
 	}
-	map[blocks[idx].center.r][blocks[idx].center.c] = idx+1;
+	map[blocks[idx].center.r][blocks[idx].center.c] = idx + 1;
 
 	if (dir == 3) { // 서쪽
 		blocks[idx].d = (blocks[idx].d + 3) % 4;
@@ -96,14 +97,14 @@ void infinite_move(int idx) {
 	// 갈수 없다면 동쪽으로 이동((dir+1)%4)
 	// 서쪽 동쪽 아래 이동 불가능 하면 종료.
 	while (1) {
-		if (move_block(idx,2)) { // 내려가기 성공
+		if (move_block(idx, 2)) { // 내려가기 성공
 			continue;
 		}
 		if (blocks[idx].exit[2].r == R + 2) break; // 바닥 도달
-		else if (move_block(idx,3)) { // 왼쪽으로 가기 성공
+		else if (move_block(idx, 3)) { // 왼쪽으로 가기 성공
 			continue;
 		}
-		else if (move_block(idx,1)) { // 오른쪽으로 가기 성공
+		else if (move_block(idx, 1)) { // 오른쪽으로 가기 성공
 			continue;
 		}
 		else break; // 내려가기 왼쪽가기 오른쪽가기 다 실패
@@ -119,13 +120,24 @@ bool check() {
 	return false;
 }
 int get_maxR(int idx) {
+	queue<int> q;
+	q.push(idx);
+	// 새로 추가되어서 길이 만들어졌다면 들어가봐야함.
 	int nowMaxR = blocks[idx].exit[2].r; // 남쪽
-	for (int i = 0; i < 4; i++) {
-		int ny = blocks[idx].exit[blocks[idx].d].r + dy[i]; // 출구방향에서 4가지 방향으로 찾기
-		int nx = blocks[idx].exit[blocks[idx].d].c + dx[i];
-		if (ny >= R + 3 || nx < 0 || nx >= C) continue;
-		if (map[ny][nx] == 0 || map[ny][nx] == idx + 1) continue;
-		nowMaxR = nowMaxR < blocks[map[ny][nx] - 1].maxV ? blocks[map[ny][nx] - 1].maxV : nowMaxR;
+	int visited[1000] = { 0 };
+	visited[idx] = 1;
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = blocks[now].exit[blocks[now].d].r + dy[i]; // 출구방향에서 4가지 방향으로 찾기
+			int nx = blocks[now].exit[blocks[now].d].c + dx[i];
+			if (ny >= R + 3 || nx < 0 || nx >= C) continue;
+			if (map[ny][nx] == 0 || map[ny][nx] == now + 1) continue;
+			if (visited[map[ny][nx] - 1]) continue;
+			visited[map[ny][nx] - 1] = 1;
+			nowMaxR = nowMaxR < blocks[map[ny][nx] - 1].maxV ? blocks[map[ny][nx] - 1].maxV : nowMaxR; // 최댓값 갱신
+			q.push(map[ny][nx] - 1);
+		}
 	}
 	blocks[idx].maxV = nowMaxR; // 최신화
 	return nowMaxR;
@@ -137,19 +149,18 @@ int main() {
 
 		info backup = blocks[tc];
 		infinite_move(tc);
-		// 만약 0~2 행에 값이 존재한다면 맵을 비우고 해당번호 다시 무한 이동 작업을함
+		// 만약 0~2 행에 값이 존재한다면 맵을 비우고 작업함.
 		if (check()) {
 			memset(map, 0, sizeof(map));
 			blocks[tc] = backup; // 데이터 초기화
-			tc--;
 			continue;
 		}
 		// 아니면 요정이 최대로 내려갈수 있는 값 계산
 			// (출구 근처에 4방향으로 검사했을때 다른 번호가 있다면 해당 번호의 최대 R 값과 현재 인덱스의 최대 R값을 비교하여 가장 큰 R값을 저장
 			// 결과값 += 현재 R값
-		int tmp = get_maxR(tc)-2;
+		int tmp = get_maxR(tc) - 2;
 		result += tmp;
 	}
 
-
+	cout<< result;
 }
